c8d7031fb553258d20ef5d2ad233b0a1715955a6
edvansabino <edvansabino@gmail.com>
2022-10-28 11:22:16 -0300

    Preparando arquivos de estudos turma 28B

---
+++ exercicios-fixacao/script.js
@@ -0,0 +1,12 @@
+const paragraph = document.getElementById("paragraph");
+      paragraph.style.color = "red";
+
+const pageTitle = document.getElementById("page-title");
+      pageTitle.innerText = "Star Wars";
+
+const secondParagraph = document.getElementById("second-paragraph");
+      secondParagraph.style.color = "green";
+      
+const modifySubtitle = document.getElementById("subtitle");
+      modifySubtitle.style.color = "orange";
+      modifySubtitle.style.backgroundColor = "black";
