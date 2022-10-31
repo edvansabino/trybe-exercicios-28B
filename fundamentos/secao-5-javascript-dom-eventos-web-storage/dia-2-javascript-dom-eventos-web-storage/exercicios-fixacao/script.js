45d26697dd6bfd77cd8f7d9c7b6c9807df32957b
edvansabino <edvansabino@gmail.com>
2022-10-31 12:20:49 -0300

    Fixando conteúdo Elementos pai/filho

--- fundamentos/secao-5-javascript-dom-eventos-web-storage/dia-2-javascript-dom-eventos-web-storage/exercicios-fixacao/script.js
+++ fundamentos/secao-5-javascript-dom-eventos-web-storage/dia-2-javascript-dom-eventos-web-storage/exercicios-fixacao/script.js
@@ -1,5 +1,18 @@
 // arquivo script.js
+// Acesse o elemento elementoOndeVoceEsta.
+let currentElement = document.querySelector('#elementoOndeVoceEsta');
+  console.log(currentElement);
 
-console.log(document.getElementById('start').nextSibling) // nó
+// Acesse pai a partir de elementoOndeVoceEsta e adicione uma color a ele.
+let elementoPai = document.querySelector('#elementoOndeVoceEsta').parentElement;
+  elementoPai.style.backgroundColor = "gray";
+  console.log(elementoPai);
+  
+// Acesse o primeiroFilhoDoFilho e adicione um texto a ele. Você se lembra dos vídeos da aula anterior, como fazer isso?
 
-console.log(document.getElementById('start').nextElementSibling) // <p>elemento</p>
+// Acesse o primeiroFilho a partir de pai.
+// Agora acesse o primeiroFilho a partir de elementoOndeVoceEsta.
+// Agora acesse o texto Atenção! a partir de elementoOndeVoceEsta.
+// Agora acesse o terceiroFilho a partir de elementoOndeVoceEsta.
+// Agora acesse o terceiroFilho a partir de pai.
+
