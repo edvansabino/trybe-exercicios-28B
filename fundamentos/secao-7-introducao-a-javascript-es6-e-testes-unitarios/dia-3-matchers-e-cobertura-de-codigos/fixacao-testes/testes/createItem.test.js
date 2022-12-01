586cbf0d8f120a9b1cb927f8be5573f1a2f9f738
edvansabino <edvansabino@gmail.com>
2022-12-01 15:55:15 -0300

    Exercícios dia 1 seção 8

---
+++ fundamentos/secao-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-matchers-e-cobertura-de-codigos/fixacao-testes/testes/createItem.test.js
@@ -0,0 +1,18 @@
+const createItem = require("../createItem");
+
+describe("a função createItem", () => {
+  it("cria um item válido", () => {
+    const expected = { name: "banana", unit: "kg", price: 1.99, quantity: 20 };
+    const actual = createItem("banana", "kg", 1.99, 20);
+    expect(actual).toEqual(expected);
+  });
+  it("utiliza zero como quantidade padrão", () => {
+    const expected = 0;
+    const actual = createItem('banana', "kg", 1.99);
+    expect(actual).toHaveProperty("quantity", expected);
+  });
+  it.todo("Lança um erro quando não recebe parâmetros");
+  it.todo("Lança um erro se o nome do item não é uma string");
+  it.todo("Lança um erro se o preço é negativo");
+  it.todo("Lança um erro se o preço é zero");
+});
---
+++ fundamentos/secao-8-higher-order-functions-do-javascript-es6/dia-1-introducao-a-higher-order-functions/script.js
@@ -0,0 +1,23 @@
+// const arrayOfValues = ['josé', 50, 0.25, { comida: 'Chocolate' }];
+
+// arrayOfValues.forEach((element, index) => {
+//   console.log(`O elemento atual é: ${element} e possui o index: ${index}`);
+// });
+
+const pessoasAprovadas = ['Ana Beatriz', 'Caio Nunes', 'Afonso Ribeiro', 'Leonardo Lins']; // lista de nomes
+
+pessoasAprovadas.forEach((nome, index) => {
+  pessoasAprovadas[index] = pessoasAprovadas[index].toUpperCase(); // acessa cada elemento do array e atualiza para letra maiúscula
+});
+
+console.log(pessoasAprovadas); // ['ANA BEATRIZ', 'CAIO NUNES', 'AFONSO RIBEIRO', 'LEONARDO LINS']
+
+// const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+// numbers.forEach((element) => {
+//   console.log(element * 2);
+// });
+// como resolver a mesma função em uma linha
+const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+numbers.forEach((element) => console.log(element * 2));
