6b8f98bc05c9a880c2bdcbb182ba007b9e580bd7
edvansabino <edvansabino@gmail.com>
2022-12-03 20:31:20 -0300

    Exercício 1 de fixação dia 8.2

--- fundamentos/secao-8-higher-order-functions-do-javascript-es6/dia-2-higher-order-functions-sort-e-map/script.js
+++ fundamentos/secao-8-higher-order-functions-do-javascript-es6/dia-2-higher-order-functions-sort-e-map/script.js
@@ -1,1 +1,13 @@
 // Exercícios do dia 2 HOF's
+const people = [
+  { name: "Mateus", age: 18 },
+  { name: "José", age: 16 },
+  { name: "Ana", age: 23 },
+  { name: "Cláudia", age: 20 },
+  { name: "Bruna", age: 19 },
+];
+
+// Adicione seu código aqui
+people.sort((person1, person2) => person1.age - person2.age);
+people.sort((person1, person2) => person2.age - person1.age);
+console.log(people);
