1b38c8739d293cfd872ec431b72e9c7c5e386e02
edvansabino <edvansabino@gmail.com>
2022-12-05 20:52:31 -0300

    Exercicio de fixacao 1 segunda opcao

--- fundamentos/secao-8-higher-order-functions-do-javascript-es6/dia-4-javascript-es6-spread-operator-rest-parameters-e-object-destructuring/ex-fixacao.js
+++ fundamentos/secao-8-higher-order-functions-do-javascript-es6/dia-4-javascript-es6-spread-operator-rest-parameters-e-object-destructuring/ex-fixacao.js
@@ -4,8 +4,15 @@ const specialFruit = ["maçã", "banana", "uva"];
 // Faça uma lista de complementos que você gostaria de adicionar
 const additionalItens = ["melancia", "melão", "morango"];
 
+//* resolução sem declaração de variável para armazenar o array.
+// const fruitSalad = (fruit, additional) => {
+//   return [...specialFruit, ...additionalItens];
+// };
+
+//* resolução com declaração de variável para armazenar o array.
 const fruitSalad = (fruit, additional) => {
-  return [...specialFruit, ...additionalItens];
+  const newFruitSalad = [...specialFruit, ...additionalItens];
+  return newFruitSalad;
 };
 
 console.log(fruitSalad(specialFruit, additionalItens));
