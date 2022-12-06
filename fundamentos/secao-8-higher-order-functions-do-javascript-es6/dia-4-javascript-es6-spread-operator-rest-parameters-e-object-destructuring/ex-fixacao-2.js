a3390f3d02ed42a88834fa7c1cc611ae92509ada
edvansabino <edvansabino@gmail.com>
2022-12-05 21:47:00 -0300

    Comentando codigo para estruturar melhor

--- fundamentos/secao-8-higher-order-functions-do-javascript-es6/dia-4-javascript-es6-spread-operator-rest-parameters-e-object-destructuring/ex-fixacao-2.js
+++ fundamentos/secao-8-higher-order-functions-do-javascript-es6/dia-4-javascript-es6-spread-operator-rest-parameters-e-object-destructuring/ex-fixacao-2.js
@@ -10,11 +10,32 @@ const jobInfos = {
   squadInitials: "RLL",
 };
 
+//* Concatenando todas as chaves em um único objeto utilizando rest parameters.
+
 const userJobInfos = { ...user, ...jobInfos };
 
-const { name, age, nationality, profession, squadInitials, squad } =
-  userJobInfos;
+//* Resolução com a desestruturação primeiro.
 
-console.log(
-  `Hi, my name is ${name}, I'm ${age} years old and I'm ${nationality}. I work as a ${profession} and my squad is ${squadInitials}-${squad}.`,
-);
+// const { name, age, nationality, profession, squadInitials, squad } =
+//   userJobInfos;
+
+// console.log(
+//   `Hi, my name is ${name}, I'm ${age} years old and I'm ${nationality}. I work as a ${profession} and my squad is ${squadInitials}-${squad}.`,
+// );
+
+//* Resolução em forma de função.
+
+const personInfos = ({
+  name,
+  age,
+  nationality,
+  profession,
+  squadInitials,
+  squad,
+}) => {
+  console.log(
+    `Hi, my name is ${name}, I'm ${age} years old and I'm ${nationality}. I work as a ${profession} and my squad is ${squadInitials}-${squad}.`,
+  );
+};
+
+personInfos(userJobInfos);
